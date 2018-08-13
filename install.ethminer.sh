eabdd133cee60586a1fc1b66b9e9838ad475c715
root <matt@dwarfsun.co.za>
2018-08-13 18:59:38 +0200

    Changes

--- install.ethminer.sh
+++ install.ethminer.sh
@@ -16,14 +16,24 @@ if [ -x cuda_9.2.148_396.37_linux ]
 then
   echo "Killing X Server"
   service lightdm stop
-  echo "Installing driver" 
-  ./cuda_9.2.148_396.37_linux --silent --override --driver
-  echo "Installing toolkit"
+  service gdm stop
+  service gdm3 stop
+  service kdm stop
+  echo "Purging extant nVidia drivers and utils"
+  apt-get --assume-yes purge nvidia*
+  apt-get --assume-yes autoremove
+  echo "Blacklisting Nouveau Driver"
+  echo "blacklist nouveau" > /etc/modprobe.d/blacklist-nouveau.conf
+  echo "options nouveau modeset=0" >> /etc/modprobe.d/blacklist-nouveau.conf
+  update-initramfs -u
+  echo "Installing driver... this may take a while" 
+  ./cuda_9.2.148_396.37_linux --silent --override --driver --no-opengl-libs --run-nvidia-xconfig
+  echo "Installing toolkit... this may take a while"
   ./cuda_9.2.148_396.37_linux --silent --override --toolkit
   echo "Done"
 fi
 
-apt-get install gcc-6 g++-6 cmake build-essential
+apt-get --assume-yes install gcc-6 g++-6 cmake build-essential
 
 git clone https://github.com/ethereum-mining/ethminer
 cd ethminer
@@ -32,3 +42,9 @@ mkdir build
 cd build
 cmake .. -DCMAKE_C_COMPILER=gcc-6 -DCMAKE_CXX_COMPILER=g++-6
 cmake --build .
+
+echo "Starting X Server"
+service lightdm start
+service gdm start
+service gdm3 start
+service kdm start
